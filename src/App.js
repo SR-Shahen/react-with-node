7a27a806ae21a4556f5e9a6cea4664b729f9adb0
SR-Shahen <s.r.shahenda@gmail.com>
2022-04-21 23:21:02 +0600

    fetch data from server side and show browser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handelFromSubmit = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email }
+    console.log(user);
+
+    // post server data
+    fetch('http://localhost:5000/user', {
+      method: "POST",
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newUsers = [...users, data]
+        setUsers(newUsers)
+        console.log(data);
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handelFromSubmit}>
+        <input type="text" name="name" id="" placeholder='enter yout name' required />
+        <input type="email" name="email" id="" placeholder='enter your email' required />
+        <input type="submit" value="submit" />
+      </form>
+      <h3>user length:{users.length}</h3>
+      {
+        users.map(user => <p key={user.id}> id: {user.id}  name: {user.name}  . Email:{user.email}</p>)
+      }
+
     </div>
   );
 }
